12d64f2531a44258c37d9352c89551567b0245d0
ayselcetin07 <ayselcetin07@hotmail.com>
2022-05-27 13:57:28 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+//package edu.erciyes.FX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) {}
+
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.fxt1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.graphics;
+
+
+    opens edu.erciyes.FX;
+
+}
